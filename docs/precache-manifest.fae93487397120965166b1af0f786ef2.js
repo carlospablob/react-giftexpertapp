34cb604ba4386a92cd25f5faac994a43710e67a4
Carlos Pablo <carlospablo.b@gmail.com>
2021-02-02 18:59:03 -0600

    Se agrega carpeta docs

---
+++ docs/precache-manifest.fae93487397120965166b1af0f786ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192cd1e4357ac95b3b0581da11ac377b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3206aa73b66788dd09a",
+    "url": "/static/css/main.f3f40d7f.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3206aa73b66788dd09a",
+    "url": "/static/js/main.1ca90f97.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
